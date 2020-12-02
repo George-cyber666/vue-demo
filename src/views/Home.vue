eb7dc9134c42d4979bd63eb0e46c828b409dbb7c
chengzhaojing <zhaojing@ideamake.cn>
2020-12-02 17:13:32 +0800

    feat: 初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div></div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+  methods: {
+    action () {
+      console.log(1);
+    }
+  }
+}
+</script>
